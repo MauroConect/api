82b1ee63c5491d8392a73b29ffd04f895876474f
MauroConect <maurot970@gmail.com>
2021-09-07 18:24:04 -0300

    inicio de desarrollo

---
+++ config/conexion.php
@@ -0,0 +1,25 @@
+<?php
+//conexion a la base de datos
+//Creo la clase para conectarme a la base de datos
+class Conexion  {
+    protected $dbh;
+    //manejo de errores
+    try {
+        $conectar= $this->dbh = new PDO ("mysql:local=localhost;dbname=api","root","")
+        return $conectar;
+    } catch (Exeption $e ) {
+
+        print "error en bd! : " . $e->getMenssage() . "<br/>";
+        die();
+
+    }
+
+    //funciones
+    public function set_names() {
+        return $this->bdh->query("SET NAMES" 'utf-8');
+    }
+}
+
+
+
+?>
